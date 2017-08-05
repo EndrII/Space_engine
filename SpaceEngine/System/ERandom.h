ccc6a2a308743912ab21b32102d37a30e66efe8d
EndrII <EndrIIMail@gmail.com>
2017-08-05 10:24:54 +0300

    fixed random

--- SpaceEngine/System/ERandom.h
+++ SpaceEngine/System/ERandom.h
@@ -8,7 +8,7 @@ class ERandom //generator sluchainih chisel
 private:
 public:
     explicit ERandom();
-    int RanBit();// 0-1 int
+    bool RanBit();// 0-1 int
     double Next(); // 0 - 1 double
     int Next(const int& range); // 0- range  range - neVkluchaetsya
     int Next(const int& rangeA, const int& rangeB); //rangeA-rangeB A- vkluchitelno B -net
